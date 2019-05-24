8fc730a6c19ebf08f16cf36434e2493c6bdb6d4b
dmwarn <dmwarner@usgs.gov>
2019-05-24 10:33:27 -0400

    Update PsiReader.r

--- R/PsiReader.r
+++ R/PsiReader.r
@@ -10,18 +10,21 @@
 #' 'EV file folder name - frequency kHz', psi, and EBA.
 #' @export
 #'
+#' @import svDialogs
+
 
   ReadPsi <- function() {
     x <- 1
     psi.df = data.frame()
-    sv$EVfolder <- sapply(strsplit(sapply(strsplit(sv$EV_filename, "[\\]"),  "[", 6), "[/]"), "[",1)
+    sv$EVfolder <- sapply(strsplit(str_extract(sv$EV_filename, pattern = 'EVfiles_baird/|EVfiles_sturgeon/|EVfiles_ltbb/'), '[/]'), "[", 1 )
+    #sv$EVfolder <- sapply(strsplit(sapply(strsplit(sv$EV_filename, "[\\]"),  "[", 6), "[/]"), "[",1)
     ev.source.freq <- unique(sv[c("EVfolder", "Frequency")])
     unique.transducer <- paste0(ev.source.freq$EVfolder, " - ", ev.source.freq$Frequency, " kHz" )
     while(x<=length(unique.transducer)) {
       df <- data.frame(dat.source = NA, psi = NA)
       dat.source <- unique.transducer[x]
       df$dat.source <- dat.source
-      df$EBA <- as.numeric(svDialogs::dlg_input(GUI = EBA, paste0("Enter Equivalent beam angle in dB for ", unique.transducer[x], ":"))$res)
+      df$EBA <- as.numeric(dlg_input(GUI = EBA, paste0("Enter Equivalent beam angle in dB for ", unique.transducer[x], ":"))$res)
       df$psi <- 10^(df$EBA/10)
       psi.df <- rbind(psi.df, df)
       x <- x+1
